5047df68d986f1752c100f863d02dac36ca50796
nurkadyr <nurkadyrurmatbekov@gmail.com>
2020-12-05 13:10:34 +0600

    fix error

--- ecomers_t7/settings.py
+++ ecomers_t7/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 try:
--- ecomers_t7/settings_dev.py
+++ ecomers_t7/settings_dev.py
@@ -1,1 +1,2 @@
 DEBUG = True
+SECRET_KEY = 'h2+a1wntqyn^ugnqff4h!z1pb0$)(ra3yc2%%i@l2=%q9=)w5x'
